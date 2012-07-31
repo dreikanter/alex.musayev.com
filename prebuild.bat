3642aa66b039a042b73d2483f9f46ae515198f1f
Alex Musayev <alex.musayev@gmail.com>
2012-07-31 14:36:54 +0400

    Bootstrap prebuild script dropped (using CDN from now).

--- prebuild.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Updating Twitter Bootstrap assets from %bootstrap_path%...
-set bootstrap_path=..\bootstrap
-set js_path=static\js
-if not exist %js_path% mkdir %js_path%
-copy /y %bootstrap_path%\js\bootstrap-collapse.js %js_path%
-copy /y %bootstrap_path%\js\bootstrap-transition.js %js_path%
